295908d5062f37d3d87bfed820783978f9f60822
Vini_Beri <99839128+ViniciusVChabariberi@users.noreply.github.com>
2023-06-14 21:55:44 -0300

    Update DiasMeses.kt

--- K14-06/DiasMeses.kt
+++ K14-06/DiasMeses.kt
@@ -1,8 +1,8 @@
 fun main(args: Array<String>) {
     var MesAtual : Int = 6
    	var Mesdias : Int = 30
-    var Dia : Int = 30
-    var DiasMeses : Int = MesAtual * Mesdias
+    var Dia : Int = 14
+    var DiasMeses : Int = ((MesAtual * Mesdias)-Mesdias)+Dia
    	var Mes: String = "Junho" 
     var Ano: Int = 2023
    
